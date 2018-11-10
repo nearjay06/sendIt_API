c84f1cb917dea59218988c4db1db4c88eb98ae26
nearjay06 <nearjoan2018@gmail.com>
2018-11-10 11:53:57 +0300

    testing to get all delivery orders

---
+++ tests/test_allorders.py
@@ -0,0 +1,18 @@
+import json
+import unittest
+from .view.endpoints import app
+
+
+class TestEndpoints(unittest.TestCase):
+  def setUp(self):
+    self.app = app.test_client()
+       
+def test_get_all_parcel_delivery_orders(self):
+ response = self.app.get('api/v1/parcels')
+ self.assertEqual(response.status_code,200)
+ self.assertEqual(response.content_type,'application/json')
+
+
+
+ if __name__== '__main__':
+  unittest.main()
