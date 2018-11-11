382a3c0bbf666d8d18e235de284bfec28245b8c8
nearjay06 <nearjoan2018@gmail.com>
2018-11-11 17:47:50 +0300

    create endpoints and tests to get all parcel delivery orders

--- view/endpoints.py
+++ view/endpoints.py
@@ -12,108 +12,123 @@ def create_delivery_order():
     parcel_name = service.get('parcel_name')
     parcel_price = service.get('parcel_price')
     delivery_time = service.get('delivery_time')
-    userId = len(delivery_orders)+1 
-          
+    userId = len(delivery_orders)+1
+    currentlocation =service.get('currentlocation')
+    destination = service.get('destination')
 
+     
     if parcel_name == " ":
-      return jsonify({"message":"you cant post an empty string"})
-    
+      return jsonify({"message":" please add parcel name"})
+      
     if not isinstance(parcel_price, int):
       return jsonify({'message':'invalid input'}),401
     
-
     if not isinstance(parcel_name,str):
-      return jsonify({"message":"parcel name should be a string"})
+      return jsonify({"message":"parcel name should be a string"}),400
     
+    if "destination" not in service:
+            return jsonify({"message":"no input for delivery destination"}),400
+
+    if "parcel_price" not in service:
+            return jsonify({"message":"invalid"}),400
+
+    if type (service["delivery_time"]) is not int:
+            return jsonify ({"message": "delivery time should not be a string"}),400     
+
+    if not destination:
+      return jsonify ({'message':'destination for delivery order required'}),400
+
+    if isinstance(destination,int):
+      return jsonify ({'message':'invalid input'}),400
+
     if delivery_time > 16:
       return jsonify({'message':'delivery should be in less than 16 hours'})
 
-    for parcel_name in delivery_orders:
-       if parcel_name in delivery_orders:
-         return jsonify(Orders.make_delivery_order()),201
-
-    order = Orders(parcelId,parcel_name,parcel_price,delivery_time,userId)
+    order = Orders(parcelId,parcel_name,parcel_price,delivery_time,userId,currentlocation,destination)
 
     delivery_orders.append(order.make_delivery_order())
     return jsonify(delivery_orders),201
 
-
 @app.route('/api/v1/parcels', methods=['GET'])
-def all_parcel_delivery_orders():
+def get_all_parcel_delivery_orders():
    if delivery_orders == []:
-      return jsonify ({"message":"delivery_orders is empty"}),400
+      return jsonify ({"message":"delivery_orders list is empty"}),400
    else:
-      return jsonify({'orders':delivery_orders}),200
+      return jsonify({'delivery_orders': delivery_orders}),200
     
           
 @app.route('/api/v1/parcels/<int:parcelId>', methods =['GET'])
-def fetch_specific_parcel_delivery_order(parcelId):
+def get_specific_parcel_delivery_order_with_ID(parcelId):
+    for order in delivery_orders:
+      if order['parcelId'] == parcelId:
+        return jsonify(order)
+      else:
+        return jsonify({'message':'delivery order not found'}),400
 
-  if delivery_orders['parcelId'] not in delivery_orders:
-    return jsonify({'message':'parcel id is not in delivery_orders'}),400
-  else:
-    if delivery_orders[parcelId] == parcelId:
-      return jsonify(Orders.make_delivery_order()),200
+      if len(delivery_orders) != 0:
+        return jsonify(order[0])
+      else:
+        return jsonify({"message":"you  dont have order in the list yet"})
 
-  for delivery_orders in delivery_orders:
-    if parcelId < 1:
-      return jsonify({'message':'order_id does not exist'}),400
+      if order['parcelId'] is None:
+        return jsonify({'message':'delivery order is not in the list'})
+
+      if order['parcelId'] is str:
+        return jsonify({'message':'parcelId cannot be a string'})
+
+      if isinstance (parcelId,str):
+        return jsonify({'message':'parcelId cannot be a string'}),400
+
+      if order['parcelId'] == parcelId:
+        return jsonify(Orders.make_delivery_order()),200
+
+      if parcelId <= 0:
+        return jsonify({'message':'parcelId does not exist'}),205
+      else:
+        return jsonify({'message':'parcelId is available'}),200
+
+  
+@app.route('/api/v1/users/<int:userId>/parcels',methods=['GET'])
+def get_delivery_order_by_specific_user_with_ID(userId):
+  for order in delivery_orders:
+    if order['userId'] == userId:
+      return jsonify(order)
     else:
-      return jsonify({'message':'order_id is available'}),200
+        return jsonify({'message':'order not found'}),400
 
-  a=0
-  for order in delivery_orders:
-    if order['parcelId'] == parcelId:
-      return jsonify ({'delivery_order': order}), 200
-    a+=1
-             
-    return jsonify({"message": 'delivery order does not exit'}), 205
+    if order['userId'] is int:
+      return jsonify(order),200
 
+    if order['userId'] is str:
+      return jsonify({'message':'userId must not be a string'}),400
 
+    if isinstance (userId,str):
+      return jsonify({'message':'userId must not be a string'}),400
 
-#Fetch all parcel delivery orders by a specific user GET /users/<userId>/parcels
-@app.route('/api/v1/users/<userId>/parcels',methods=['GET'])
-def get_delivery_order_by_specific_user(userId):
-  for order in delivery_orders:
-    pass
-
-
-
-
-
-
-
-
-
-
-@app.route('/api/v1/parcels/<int:parcelId>',methods=['PUT'])
-def cancel_specific_delivery_order(parcelId):
+@app.route('/api/v1/parcels/<int:parcelId>',methods=['DELETE'])
+def cancel_specific_delivery_order_with_ID(parcelId):
     service = request.get_json()
-    
-    parcelId = service.get('parcelId')
+
+    parcelId = len(delivery_orders)+1
     parcel_name = service.get('parcel_name')
     parcel_price = service.get('parcel_price')
     delivery_time = service.get('delivery_time')
-    userId = service.get('userId')  
+    userId = len(delivery_orders)+1
+    currentlocation =service.get('currentlocation')
+    destination = service.get('destination')
 
-    if not parcel_name:
-           return jsonify({
-                   'message': 'sorry!parcel_name is required'
-           }),400
+    order = 0
+    for order['parcelId'] in delivery_orders:
+      if order['parcelId'] == parcelId:
+         del delivery_orders[order] 
+      return jsonify ({'message':'delivery order has been terminated'}), 410
 
-    a=0
-    for parcelId in delivery_orders:
-      if delivery_orders['parcelId'] == parcelId:
-        del delivery_orders[a] 
-        return jsonify ({'message':'delivery order terminated'}), 200
-      a+=1
-             
-    return jsonify({"message": 'delivery order does not exit'}), 205
+      order +=1 
+      return jsonify({"message": 'delivery order does not exit'}), 205
 
 
 
+    
 
-
-
-
+   
 
--- view/get_allorders.py
+++ view/get_allorders.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 delivery_orders=[]
 
 @app.route('/api/v1/parcels', methods=['GET'])
-def all_parcel_delivery_orders():
+def get_all_parcel_delivery_orders():
    if delivery_orders == []:
-      return jsonify ({"message":"delivery_orders is empty"}),400
+      return jsonify ({"message":"delivery_orders list is empty"}),400
    else:
-      return jsonify({'orders':delivery_orders}),200
+      return jsonify({'delivery_orders': delivery_orders}),200
+    
+
     
