84d187831ef9885a632e19ba31e1a34c7eb54d69
nearjay06 <nearjoan2018@gmail.com>
2018-11-11 18:16:34 +0300

    create and test endpoints to get an order with ID

--- tests/test_specificorder.py
+++ tests/test_specificorder.py
@@ -2,13 +2,15 @@ import unittest
 from view.endpoints import app,create_delivery_order,get_specific_parcel_delivery_order_with_ID
 import json
 
-
-def test_get_specific_parcel_delivery_order_with_ID(self):
+class TestEndpoints(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+       
+    def test_get_specific_parcel_delivery_order_with_ID(self):
         response =self.app.get('/api/v1/parcels/1')
         self.assertEqual(response.status_code,200)
 
 
 
-
 if __name__== '__main__':
-  unittest.main()         
+ unittest.main()
--- view/specific_order.py
+++ view/specific_order.py
@@ -5,24 +5,32 @@ app = Flask(__name__)
 delivery_orders=[]
 
 @app.route('/api/v1/parcels/<int:parcelId>', methods =['GET'])
-def fetch_specific_parcel_delivery_order(parcelId):
+def get_specific_parcel_delivery_order_with_ID(parcelId):
+    for order in delivery_orders:
+      if order['parcelId'] == parcelId:
+        return jsonify(order)
+      else:
+        return jsonify({'message':'delivery order not found'}),400
 
-  if delivery_orders['parcelId'] not in delivery_orders:
-    return jsonify({'message':'parcel id is not in delivery_orders'}),400
-  else:
-    if delivery_orders[parcelId] == parcelId:
-      return jsonify(Orders.make_delivery_order()),200
+      if len(delivery_orders) != 0:
+        return jsonify(order[0])
+      else:
+        return jsonify({"message":"you  dont have order in the list yet"})
 
-  for delivery_orders in delivery_orders:
-    if parcelId < 1:
-      return jsonify({'message':'order_id does not exist'}),400
-    else:
-      return jsonify({'message':'order_id is available'}),200
+      if order['parcelId'] is None:
+        return jsonify({'message':'delivery order is not in the list'})
 
-  a=0
-  for order in delivery_orders:
-    if order['parcelId'] == parcelId:
-      return jsonify ({'delivery_order': order}), 200
-    a+=1
-             
-    return jsonify({"message": 'delivery order does not exit'}), 205
+      if order['parcelId'] is str:
+        return jsonify({'message':'parcelId cannot be a string'})
+
+      if isinstance (parcelId,str):
+        return jsonify({'message':'parcelId cannot be a string'}),400
+
+      if order['parcelId'] == parcelId:
+        return jsonify(Orders.make_delivery_order()),200
+
+      if parcelId <= 0:
+        return jsonify({'message':'parcelId does not exist'}),205
+      else:
+        return jsonify({'message':'parcelId is available'}),200
+
