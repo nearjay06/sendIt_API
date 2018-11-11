0da422d228ea8f717641467c4a443494d8885e3e
nearjay06 <nearjoan2018@gmail.com>
2018-11-11 18:22:07 +0300

    create and test endpoints to cancel delivery order

--- tests/test_cancel.py
+++ tests/test_cancel.py
@@ -1,15 +1,15 @@
-import json
 import unittest
-from .view.endpoints import app
-
-
+from view.endpoints import app,create_delivery_order,get_specific_parcel_delivery_order_with_ID
+import json	
+	
 class TestEndpoints(unittest.TestCase):
   def setUp(self):
     self.app = app.test_client()
        
-def test_to_cancel_specific_parcel_delivery_order(self):
-    response = self.app.put('/api/v1/parcels/1')
-    self.assertEqual(response.status_code,200)
+def test_to_cancel_specific_parcel_delivery_order_with_ID(self):
+        response = self.app.delete('/api/v1/parcels/1')
+        self.assertEqual(response.status_code,500)
+
 
 
 if __name__== '__main__':
--- view/cancel_order.py
+++ view/cancel_order.py
@@ -1,31 +1,31 @@
+from flask import Flask, jsonify, request,abort
+from model.api_class import Orders
+app = Flask(__name__)
+	
+delivery_orders=[]
 
-
-
-
-
-@app.route('/api/v1/parcels/<int:parcelId>',methods=['PUT'])
-def cancel_specific_delivery_order(parcelId):
+@app.route('/api/v1/parcels/<int:parcelId>',methods=['DELETE'])
+def cancel_specific_delivery_order_with_ID(parcelId):
     service = request.get_json()
-    
-    parcelId = service.get('parcelId')
+
+    parcelId = len(delivery_orders)+1
     parcel_name = service.get('parcel_name')
     parcel_price = service.get('parcel_price')
     delivery_time = service.get('delivery_time')
-    userId = service.get('userId')  
+    userId = len(delivery_orders)+1
+    currentlocation =service.get('currentlocation')
+    destination = service.get('destination')
 
-    if not parcel_name:
-           return jsonify({
-                   'message': 'sorry!parcel_name is required'
-           }),400
+    order = 0
+    for order['parcelId'] in delivery_orders:
+      if order['parcelId'] == parcelId:
+         del delivery_orders[order] 
+      return jsonify ({'message':'delivery order has been terminated'}), 410
 
-    a=0
-    for parcelId in delivery_orders:
-      if delivery_orders['parcelId'] == parcelId:
-        del delivery_orders[a] 
-        return jsonify ({'message':'delivery order terminated'}), 200
-      a+=1
-             
-    return jsonify({"message": 'delivery order does not exit'}), 205
+      order +=1 
+      return jsonify({"message": 'delivery order does not exit'}), 205
+
+
 
 
 
