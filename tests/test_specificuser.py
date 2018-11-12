82b5b4e7b33e391304c4ffe085d87e7d5ba481fd
nearjay06 <nearjoan2018@gmail.com>
2018-11-12 20:44:11 +0300

    modify tests,endpoints and debug
    [#161745992]

--- tests/test_specificuser.py
+++ tests/test_specificuser.py
@@ -1,13 +1,13 @@
 import json
 import unittest
-from view.endpoints import app
+from api.view.endpoints import app
 
 
 class TestEndpoints(unittest.TestCase):
   def setUp(self):
     self.app = app.test_client()
 
-def test_get_parcel_delivery_order_by_specific_user(self):
+def test_get_parcel_delivery_order_by_specific_user():
   pass
 
 
